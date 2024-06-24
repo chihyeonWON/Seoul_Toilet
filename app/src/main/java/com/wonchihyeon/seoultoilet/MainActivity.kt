bc7490d1945b2c29857824c03e9ac8134f7267ee
ylose <heungeob1003@gmail.com>
2024-06-24 11:10:59 +0900

    위치 권한 확인, 위치 초기화

--- app/src/main/java/com/wonchihyeon/seoultoilet/MainActivity.kt
+++ app/src/main/java/com/wonchihyeon/seoultoilet/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.wonchihyeon.seoultoilet
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
+import com.wonchihyeon.seoultoilet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,8 +35,95 @@ class MainActivity : AppCompatActivity() {
     // 구글 맵 객체를 참조할 변수
     var googleMap: GoogleMap? = null
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // 맵뷰에 onCreate 함수 호출
+        binding.mapView.onCreate(savedInstanceState)
+
+        // 앱이 실행될때 런타임에서 위치 서비스 관련 권한체크
+        if (hasPermission()) {
+            // 권한이 있는 경우 맵 초기화
+            initMap()
+        } else {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
+        }
+    }
+
+    // 권한 요청의 결과로 맵 초기화
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // 맵 초기화
+        initMap()
+    }
+
+    // 앱에서 사용하는 권한이 있는지 체크하는 함수
+    fun hasPermission(): Boolean {
+        // 퍼미션 목록중 하나라도 권한이 없으면 false 반환
+        for (permission in PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    // 맵 초기화하는 함수
+    @SuppressLint("MissingPermission")
+    fun initMap() {
+        // 맵뷰에서 구글 맵을 불러오는 함수, 콜백함수에서 구글 맵 객체가 전달됨
+        binding.mapView.getMapAsync {
+            // 구글맵 멤버 변수에 구글맵 객체 저장
+            googleMap = it
+
+            // 현재 위치로 이동 버튼 활성화
+            it.uiSettings.isMyLocationButtonEnabled = false
+
+            // 위치 권한이 있는 경우
+            when {
+                hasPermission() -> {
+                    // 현재위치 표시 활성화
+                    it.isMyLocationEnabled = true
+                    // 현재위치로 카메라 이동
+                    it.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            getMyLocation(), DEFAULT_ZOOM_LEVEL
+                        )
+                    )
+                }
+
+                else -> {
+                    // 권한이 없으면 서울시청의 위치로 이동
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, DEFAULT_ZOOM_LEVEL))
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getMyLocation(): LatLng {
+        // 위치를 측정하는 프로바이더를 GPS 센서로 지정
+        val locationProvider: String = LocationManager.GPS_PROVIDER
+        // 위치 서비스 객체를 불러옴
+        val locationManager =
+            getSystemService(android.content.Context.LOCATION_SERVICE) as LocationManager
+        // 마지막으로 업데이트된 위치를 가져옴
+        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(locationProvider)
+        // 위도 경도 객체로 변환
+        return LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
     }
 }
