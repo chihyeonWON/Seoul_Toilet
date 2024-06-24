07dcde01ee6cf09ab426c85c862fcfe3f7ab7de1
ylose <heungeob1003@gmail.com>
2024-06-24 10:27:14 +0900

    구글 맵을 띄우는 데 필요한 변수 추가(권한 목록, 퍼미션코드, 줌레벨, 기본 위치, 구글 맵 객체)

--- app/src/main/java/com/wonchihyeon/seoultoilet/MainActivity.kt
+++ app/src/main/java/com/wonchihyeon/seoultoilet/MainActivity.kt
@@ -2,8 +2,30 @@ package com.wonchihyeon.seoultoilet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity() {
+
+    // 런타임에서 권한이 필요한 퍼미션 목록
+    val PERMISSIONS = arrayOf(
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    // 퍼미션 승인 요청 시 사용하는 요청 코드
+    val REQUEST_PERMISSION_CODE = 1
+
+    // 기본 맵 줌 레벨
+    val DEFAULT_ZOOM_LEVEL = 17f
+
+    // 현재위치를 가져올 수 없는 경우 서울 시청의 위치로 지도를 보여주기 위해 서울시청의 위치를 변수로 선언
+    // LatLng 클래스는 위도와 경도를 가지는 클래스
+    val CITY_HALL = LatLng(37.56662952, 126.97794509999994)
+
+    // 구글 맵 객체를 참조할 변수
+    var googleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
